d735635ae1d7e1de1ff6df9acc2b33735595d97e
心隨緣動 <88259403+xeefei@users.noreply.github.com>
2024-05-31 23:21:28 +0800

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -944,12 +944,15 @@ warp_cloudflare() {
 }
 
 subconverter() {
+        ipv4=$(curl -s4m8 ip.p3terx.com -k | sed -n 1p)
         bash <(curl -fsSL https://get.docker.com | bash -s docker) && (docker run -d --name subweb --restart always \
                           -p 18080:80 \
+                          -p 25500:25500 \
+                          -e API_URL=http://$ipv4 \
                           stilleshan/subweb)
     echo -e "${yellow}【链接转换模块】安装完成！！！"
-    ipv4=$(curl -s4m8 ip.p3terx.com -k | sed -n 1p)
     echo -e "${green}【订阅转换功能】访问地址为：${plain}${green}http://$ipv4:18080"
+    echo -e "${green}【后端服务】拉取地址为：${plain}${green}http://$ipv4:25500"
     show_menu
 }
 
