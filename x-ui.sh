a0a31a957da0cb106771eae6438d464ee8e9ef4c
心隨緣動 <88259403+xeefei@users.noreply.github.com>
2024-06-06 14:45:56 +0800

    Update version to v2.3.4.5
    Update version to v2.3.4.5

--- x-ui.sh
+++ x-ui.sh
@@ -947,10 +947,8 @@ subconverter() {
         bash <(curl -fsSL https://get.docker.com | bash -s docker) 
         ipv4=$(curl -s4m8 ip.p3terx.com -k | sed -n 1p)
         docker run -d --name subweb --restart always \
-                          listen 25500 \
                           -p 18080:80 \
-                          -p 25500:25500 \
-                          -e API_URL=http://$ipv4:25500 \
+                          -p 25500:25500 \ 
                           stilleshan/subweb
     echo -e "${yellow}【链接转换模块】安装完成！！！"
     echo -e "${green}【订阅转换功能】访问地址为：${plain}${green}http://$ipv4:18080"
