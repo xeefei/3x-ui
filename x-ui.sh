9f0708e2ecf93c84f262d4aa73f4dd0fa410be58
心隨緣動 <88259403+xeefei@users.noreply.github.com>
2024-06-07 23:49:47 +0800

    Update version to v2.3.5
    Update version to v2.3.5

--- x-ui.sh
+++ x-ui.sh
@@ -144,7 +144,7 @@ install() {
 }
 
 update() {
-    confirm "该功能将强制安装最新版本，并且数据不会丢失。你想继续吗?" "y"
+    confirm "$(echo -e "${green}该功能将强制安装最新版本，并且数据不会丢失。${red}你想继续吗？${plain} ---->>")" "y"
     if [[ $? != 0 ]]; then
         LOGE "已取消"
         if [[ $# == 0 ]]; then
