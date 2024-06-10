b9e9c783b257b7ed7d38dea77e85453a09486088
心隨緣動 <88259403+xeefei@users.noreply.github.com>
2024-06-10 19:57:05 +0800

    Update version to v2.3.5.1
    Update version to v2.3.5.1

--- main.go
+++ main.go
@@ -184,9 +184,9 @@ func showSetting(show bool) {
                 fmt.Println("")
   // 根据条件打印带颜色的字符串
         if ipv4 != "" {
-		formattedIPv4 := fmt.Sprintf("%s %s:%s/%s" + Reset,
+		formattedIPv4 := fmt.Sprintf("%s http://%s:%s%s" + Reset,
 			Green+"面板 IPv4 访问地址------>>",
-		  //	Yellow+"http://",
+		  	Yellow+"http://",
 			Yellow+ipv4,
 			Yellow+fmt.Sprintf("%d", port),
 			Yellow+webBasePath)
@@ -194,9 +194,9 @@ func showSetting(show bool) {
 	}
 
 	if ipv6 != "" {
-		formattedIPv6 := fmt.Sprintf("%s %s:%s/%s" + Reset,
+		formattedIPv6 := fmt.Sprintf("%s http://%s:%s%s" + Reset,
 			Green+"面板 IPv6 访问地址------>>",
-		   //	Yellow+"http://",
+		   	Yellow+"http://",
 			Yellow+ipv6,
 			Yellow+fmt.Sprintf("%d", port),
 			Yellow+webBasePath)
