92dac43e27ae5fb219d93ae0377541e27dfd9c91
心隨緣動 <88259403+xeefei@users.noreply.github.com>
2024-06-14 01:00:33 +0800

    Update version to v2.3.5.3
    Update version to v2.3.5.3

--- install.sh
+++ install.sh
@@ -178,7 +178,7 @@ install_x-ui() {
         fi
         echo ""
         echo -e "-----------------------------------------------------"
-        echo -e "${green}--------->>获取 3x-ui 最新版本：${last_version}，开始安装...${plain}"
+        echo -e "${green}--------->>获取 3x-ui 最新版本：${yellow}${last_version}，开始安装...${plain}"
         echo -e "-----------------------------------------------------"
         echo ""
         wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch).tar.gz https://github.com/xeefei/3x-ui/releases/download/${last_version}/x-ui-linux-$(arch).tar.gz
